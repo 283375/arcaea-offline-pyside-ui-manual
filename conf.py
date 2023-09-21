f4640edb62eff7c4d7e03fe0876bace3aef9f16a
283375 <bilibili283375@163.com>
2023-09-22 01:26:04 +0800

    style: conf.py

--- conf.py
+++ conf.py
@@ -6,28 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Arcaea Offline PySide UI'
-copyright = 'CC BY-NC-SA 4.0'
-author = '283375'
-release = 'v0.3.0'
+project = "Arcaea Offline PySide UI"
+copyright = "CC BY-NC-SA 4.0"
+author = "283375"
+release = "v0.3.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ["sphinx.ext.autodoc", "myst_parser"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "venv"]
 
-language = 'zh_CN'
+language = "zh_CN"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
 
 # -- Internalization ---------------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/advanced/intl.html
 
-locale_dirs = ['locale/']
+locale_dirs = ["locale/"]
